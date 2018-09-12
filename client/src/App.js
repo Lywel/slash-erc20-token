b5ab201c3ace90c4e3a8ac37f7c8f4ec7421e097
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-09-12 05:33:08 +0200

    App.js: transfer function

--- client/src/App.js
+++ client/src/App.js
@@ -29,15 +29,13 @@ class App extends Component {
 
       this.setState({ web3, accounts, contract: instance }, this.getBalances);
     } catch (error) {
-      this.setState({ error:
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      })
+      this.setState({ error: error.toString() })
       console.error(error)
     }
   }
 
   getBalances = async () => {
-    const { accounts, contract } = this.state;
+    const { accounts } = this.state;
     //await contract.set(5, { from: accounts[0] });
     //const response = await contract.get();
     //this.setState({ storageValue: response.toNumber() });
@@ -47,17 +45,24 @@ class App extends Component {
   }
 
   computeBalance = async (address) => {
-    const { web3, contract } = this.state;
-    //const weiBalance = await web3.eth.getBalance(address)
-    //return web3.utils.fromWei(`${weiBalance}`, 'ether');
-    return await contract.balances(address)
+    const { web3 } = this.state;
+    const weiBalance = await web3.eth.getBalance(address)
+    return web3.utils.fromWei(`${weiBalance}`, 'ether');
   }
 
   mintAmount = async () => {
-    const { web3, contract, accounts } = this.state
+    const { contract, accounts } = this.state
     await contract.mint(accounts[0], this.state.mintAmount, { from: accounts[0] });
     //const response = await contract.get();
     //this.setState({ storageValue: response.toNumber() });
+  }
+
+  sentAmount = async () => {
+    const { web3, contract, accounts } = this.state
+    await contract.send(accounts[0], {
+        from: accounts[0],
+        value: web3.toWei(`${this.state.transferAmount}`, 'ether')
+      });
   }
 
   handleMintAmountChange = (e) => {
@@ -72,10 +77,72 @@ class App extends Component {
 
 
   render() {
-    if (!this.state.balances || !this.state.accounts) {
+    const error = (this.state.error
+      ? (<Alert color="danger" hidden={ !this.state.error }>
+            { this.state.error }
+         </Alert>)
+      : null)
+
+    const page = ((this.state.balances && this.state.accounts)
+      ? (
+      <div>
+        <Row>
+          <Col md="3">
+            <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+              <CardTitle>Your wallet</CardTitle>
+              <CardBody>
+                <FormGroup>
+                  <Label for="transferAmount">Balance:</Label>
+                  <Input type="text" disabled value={ this.state.balances[0] } />
+                </FormGroup>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="4">
+            <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+              <CardTitle>Mint</CardTitle>
+              <CardBody>
+                <FormGroup>
+                  <Label for="mintAmount">Amount:</Label>
+                  <Input type="number" name="mintAmount"
+                    value={this.state.mintAmount}
+                    onChange={this.handleMintAmountChange} />
+                </FormGroup>
+                <Button color="success" onClick={this.mintAmount}>Mint</Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="5">
+            <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+              <CardTitle>Transfer</CardTitle>
+              <CardBody>
+                <FormGroup>
+                  <Label for="transferTo">Destination:</Label>
+                  <Input type="text" name="transferTo" id="transferTo" placeholder="0x0000..." />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="transferAmount">Amount:</Label>
+                  <Input type="number" name="transferAmount" id="transferAmount" placeholder="0" />
+                </FormGroup>
+                <Button color="success" onClick={this.sendAmount}>Send</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <small className="text-muted">Account:  { this.state.accounts[0] }</small>
+      </div>
+      )
+      : null)
+
+    if ((!this.state.balances || !this.state.accounts) && !error) {
       return (
-        <img src="//alwahda-mall.com/images/loading.gif"
+        <Container>
+          <Alert color="info" style={{ marginTop: 15 }}>
+            Connecting to the private network...
+          </Alert>
+          <img src="//alwahda-mall.com/images/loading.gif"
              className="rounded mx-auto d-block" />
+        </Container>
       );
     }
 
@@ -83,53 +150,8 @@ class App extends Component {
       <div className="App">
         <Container>
           <h1>Slash coins</h1>
-          <Alert color="danger" hidden={ !this.state.error }>
-            { this.state.error }
-          </Alert>
-          <Row>
-            <Col md="3">
-              <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                <CardTitle>Your wallet</CardTitle>
-                <CardBody>
-                  <FormGroup>
-                    <Label for="transferAmount">Balance:</Label>
-                    <Input type="text" disabled value={ this.state.balances[0] } />
-                  </FormGroup>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="4">
-              <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                <CardTitle>Mint</CardTitle>
-                <CardBody>
-                  <FormGroup>
-                    <Label for="mintAmount">Amount:</Label>
-                    <Input type="number" name="mintAmount"
-                      value={this.state.mintAmount}
-                      onChange={this.handleMintAmountChange} />
-                  </FormGroup>
-                  <Button color="success" onClick={this.mintAmount}>Mint</Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="5">
-              <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                <CardTitle>Transfer</CardTitle>
-                <CardBody>
-                  <FormGroup>
-                    <Label for="transferTo">Destination:</Label>
-                    <Input type="text" name="transferTo" id="transferTo" placeholder="0x0000..." />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="transferAmount">Amount:</Label>
-                    <Input type="number" name="transferAmount" id="transferAmount" placeholder="0" />
-                  </FormGroup>
-                  <Button color="success">Send</Button>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-          <small className="text-muted">Account:  { this.state.accounts[0] }</small>
+          { !!error && error }
+          { !!page && page }
         </Container>
       </div>
     );
